f0c8ef479574cc857f30dc7a7c939cae4eff2cd4
steve he <steve@localhost.localdomain>
2025-12-01 12:18:48 -0500

    added env vars and start script

---
+++ wiki/start-wiki.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ip route get 8.8.8.8 > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+	echo "on routable IP network"
+	export EXT_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+else
+	echo "on host-only network, using eth0 device for primary IP"
+	export EXT_IP=$(ip route | grep eth0 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+fi
+
+docker compose up -d
